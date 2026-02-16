f431c7eabc0310cb79a8d50a17cb89fde652b21a
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-02-16 22:20:31 +0530

    find peak element

---
+++ Arrays/Q107.java
@@ -0,0 +1,23 @@
+/*
+Find Peak Element
+ */
+
+public class Q107 {
+    public static int findPeakElement(int[] nums) {
+        int start=0; int end=nums.length-1;
+        while(start<end) {
+            int mid=start+(end-start)/2;
+
+            if(nums[mid]>nums[mid+1]) {
+                end=end-1;
+            } else if(nums[mid]<nums[mid+1]) {
+                start=start+1;
+            }
+        }
+        return start;
+    }
+    public static void main(String[] args) {
+        int[] nums={8 ,12 ,18, 20, 17, 5, 1};
+        System.out.println(findPeakElement(nums));
+    }
+}

2733107a0a03974b05e381a739928b75e6543be9
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-02-19 20:18:16 +0530

    3sum

---
+++ Arrays/Q112.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Q112 {
+        public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums); // Step 1: Sort the array
+        List<List<Integer>> result = new ArrayList<>();
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate values for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum < 0) {
+                    left++;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    // Skip duplicates for left
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    // Skip duplicates for right
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    
+                    left++;
+                    right--;
+                }
+            }
+        }
+        
+        return result;
+    }
+
+    // Quick test
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
+    }
+}

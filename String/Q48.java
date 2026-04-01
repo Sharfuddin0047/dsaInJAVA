415f95771c071e3649c70d07302cee87b14424e3
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-04-01 23:02:01 +0530

    Backspace String Compare

---
+++ String/Q48.java
@@ -0,0 +1,28 @@
+package String;
+
+/*Backspace String Compare */
+public class Q48 {
+    public static boolean backspaceCompare(String s, String t) {
+        return build(s).equals(build(t));
+    }
+
+    public static String build(String str) {
+        StringBuilder sb = new StringBuilder();
+
+        for(char x:str.toCharArray()) {
+            if(x=='#') {
+                if(sb.length()>0) {
+                    sb.deleteCharAt(sb.length()-1);
+                }
+            } else {
+                sb.append(x);
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        System.out.println(obj.backspaceCompare("ab#c", "ad#c")); 
+        System.out.println(obj.backspaceCompare("ab##", "c#d#")); 
+        System.out.println(obj.backspaceCompare("a#c", "b")); 
+    }
+}

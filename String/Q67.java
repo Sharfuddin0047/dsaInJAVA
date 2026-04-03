09d3d9e6683b879f7580a3cab638f524a715db18
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-04-04 00:26:58 +0530

    print odd length word

---
+++ String/Q67.java
@@ -0,0 +1,26 @@
+package String;
+
+
+/*
+WAJP take a String input and print and count all
+the words which has odd number of characters.
+*/
+public class Q67 {
+    public static void printAndCount(String str) {
+        int count = 0;
+        String[] arr = str.split("\\s+"); 
+        for (String word : arr) {
+            if (word.length() % 2 != 0) {
+                count++;
+                System.out.println(word);
+            }
+        }
+
+        System.out.println("Total words with even length: " + count);
+    }
+
+    public static void main(String[] args) {
+        printAndCount("This is a simple test string");
+        
+    }
+}

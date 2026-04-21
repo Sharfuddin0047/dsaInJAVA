0c754bff94ef1aaf8496638710010016629fe7a3
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-04-21 21:36:22 +0530

    Sort Characters By Frequency

---
+++ String/Q72.java
@@ -0,0 +1,40 @@
+package String;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Q72 {
+    public static String frequencySort(String s) {
+    
+        Map<Character, Integer> freqMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+
+        
+        PriorityQueue<Map.Entry<Character, Integer>> maxHeap =
+                new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+
+        maxHeap.addAll(freqMap.entrySet());
+
+        
+        StringBuilder sb = new StringBuilder();
+        while (!maxHeap.isEmpty()) {
+            Map.Entry<Character, Integer> entry = maxHeap.poll();
+            char c = entry.getKey();
+            int count = entry.getValue();
+            for (int i = 0; i < count; i++) {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "tree";
+        System.out.println(frequencySort(s)); 
+    }
+}
+

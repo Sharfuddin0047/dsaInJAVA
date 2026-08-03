a50909ea68935a79f377b64c7a133021d3f9d401
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2026-08-03 17:45:30 +0530

    performe linkedlist addition last

--- collections/linkedlist/LinkedListPractice.java
+++ collections/linkedlist/LinkedListPractice.java
@@ -13,7 +13,7 @@ public class LinkedListPractice {
     public static Node head;
     public static Node tail;
 
-
+    //  Add First in linkedlist
     public void addFirst(int data) {
         //step 1 = create new node
         Node newNode = new Node(data);
@@ -29,11 +29,28 @@ public class LinkedListPractice {
         //step3 - head = newNode
         head = newNode;
     } 
+
+
+    // Add Last in linkedlist
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        tail.next = newNode;
+        tail = newNode;
+    }
     public static void main(String[] args) {
         LinkedListPractice ll = new LinkedListPractice();
 
         ll.addFirst(1);
         ll.addFirst(2); 
+
+        ll.addLast(3);
+        ll.addLast(4);
 
     }
 }
